2f177fca5e8908049d15629c54868cb2f30e710b
Asma Hamid <asmahamid1382@gmail.com>
2026-06-10 00:28:56 +0330

    db

--- internal/database/db.go
+++ internal/database/db.go
@@ -1,58 +1,57 @@
 package database
 
-import (
-    "fmt"
-    "log"
-
-    "gorm.io/driver/sqlite"
-    "gorm.io/gorm"
-)
-
-var DB *gorm.DB
-
-func Connect() {
-    var err error
-
-    DB, err = gorm.Open(sqlite.Open("live_commerce.db"), &gorm.Config{})
-    if err != nil {
-        log.Fatal("❌ Failed to connect to database:", err)
-    }
-
-	if err := DB.Exec("PRAGMA foreign_keys = ON;").Error; err != nil {
-		log.Fatal("❌ Failed to enable foreign keys:", err)
-	}
-
-    fmt.Println("✅ SQLite database connected successfully")
-}
-
-
-// package database
-
 // import (
-// 	"fmt"
-// 	"log"
-// 	"os"
+//     "fmt"
+//     "log"
 
-// 	"gorm.io/driver/postgres"
-// 	"gorm.io/gorm"
+//     "gorm.io/driver/sqlite"
+//     "gorm.io/gorm"
 // )
 
 // var DB *gorm.DB
 
 // func Connect() {
-// 	var err error
-// 	dsn := fmt.Sprintf(
-// 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-// 		os.Getenv("DB_HOST"),
-// 		os.Getenv("DB_USER"),
-// 		os.Getenv("DB_PASSWORD"),
-// 		os.Getenv("DB_NAME"),
-// 		os.Getenv("DB_PORT"),
-// 	)
+//     var err error
 
-// 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-// 	if err != nil {
-// 		log.Fatal("❌ Failed to connect to database:", err)
+//     DB, err = gorm.Open(sqlite.Open("live_commerce.db"), &gorm.Config{})
+//     if err != nil {
+//         log.Fatal("❌ Failed to connect to database:", err)
+//     }
+
+// 	if err := DB.Exec("PRAGMA foreign_keys = ON;").Error; err != nil {
+// 		log.Fatal("❌ Failed to enable foreign keys:", err)
 // 	}
-// 	fmt.Println("✅ Database connected successfully")
+
+//     fmt.Println("✅ SQLite database connected successfully")
 // }
+
+
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	var err error
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("❌ Failed to connect to database:", err)
+	}
+	fmt.Println("✅ Database connected successfully")
+}
